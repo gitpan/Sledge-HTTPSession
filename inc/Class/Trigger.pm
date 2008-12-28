05d98f296a9531cd38b15f5787a37f6b85eafd49
KAN Fushihara <kan.fushihara@gmail.com>
2008-12-28 08:24:06 +0000

    Import of MIKIHOSHI/Sledge-HTTPSession-0.04 from CPAN.
    gitpan-cpan-distribution: Sledge-HTTPSession
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MIKIHOSHI/Sledge-HTTPSession-0.04.tar.gz
    gitpan-cpan-author:       MIKIHOSHI
    gitpan-cpan-maturity:     released

--- inc/Class/Trigger.pm
+++ inc/Class/Trigger.pm
@@ -3,141 +3,99 @@ package Class::Trigger;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = "0.12";
+$VERSION = "0.10";
 
+use Class::Data::Inheritable;
 use Carp ();
-
-my (%Triggers, %TriggerPoints);
 
 sub import {
     my $class = shift;
     my $pkg = caller(0);
 
-    $TriggerPoints{$pkg} = { map { $_ => 1 } @_ } if @_;
+    # XXX 5.005_03 isa() is broken with MI
+    unless ($pkg->can('mk_classdata')) {
+	no strict 'refs';
+	push @{"$pkg\::ISA"}, 'Class::Data::Inheritable';
+    }
+
+    $pkg->mk_classdata('__triggers');
+    $pkg->mk_classdata('__triggerpoints');
+
+    $pkg->__triggerpoints({ map { $_ => 1 } @_ }) if @_;
 
     # export mixin methods
     no strict 'refs';
-    my @methods = qw(add_trigger call_trigger last_trigger_results);
+    my @methods = qw(add_trigger call_trigger);
     *{"$pkg\::$_"} = \&{$_} for @methods;
 }
 
 sub add_trigger {
     my $proto = shift;
 
-    my $triggers = __fetch_triggers($proto);
-
-    my %params = @_;
-    my @values = values %params;
-    if (@_ > 2 && (grep { ref && ref eq 'CODE' } @values) == @values) {
-        Carp::croak "mutiple trigger registration in one add_trigger() call is deprecated.";
+    # should be deep copy of the hash: for inheritance
+    my $old_triggers = __fetch_triggers($proto) || {};
+    my %triggers = __deep_dereference($old_triggers);
+    while (my($when, $code) = splice @_, 0, 2) {
+	__validate_triggerpoint($proto, $when);
+	Carp::croak('add_trigger() needs coderef') unless ref($code) eq 'CODE';
+	push @{$triggers{$when}}, $code;
     }
-
-    if ($#_ == 1 && ref($_[1]) eq 'CODE') {
-        @_ = (name => $_[0], callback => $_[1]);
-    }
-
-    my %args = ( name => undef, callback => undef, abortable => undef, @_ );
-    my $when = $args{'name'};
-    my $code = $args{'callback'};
-    my $abortable = $args{'abortable'};
-    __validate_triggerpoint( $proto, $when );
-    Carp::croak('add_trigger() needs coderef') unless ref($code) eq 'CODE';
-    push @{ $triggers->{$when} }, [ $code, $abortable ];
-
-    1;
-}
-
-
-sub last_trigger_results {
-    my $self = shift;
-    my $result_store = ref($self) ? $self : ${Class::Trigger::_trigger_results}->{$self};
-    return $result_store->{'_class_trigger_results'};
+    __update_triggers($proto, \%triggers);
 }
 
 sub call_trigger {
     my $self = shift;
+    return unless my $all_triggers = __fetch_triggers($self); # any triggers?
     my $when = shift;
-
-    my @return;
-
-    my $result_store = ref($self) ? $self : ${Class::Trigger::_trigger_results}->{$self};
-
-    $result_store->{'_class_trigger_results'} = [];
-
-    if (my @triggers = __fetch_all_triggers($self, $when)) { # any triggers?
-        for my $trigger (@triggers) {
-            my @return = $trigger->[0]->($self, @_);
-            push @{$result_store->{'_class_trigger_results'}}, \@return;
-            return undef if ($trigger->[1] and not $return[0]); # only abort on false values.
-        }
+    if (my $triggers = $all_triggers->{$when}) {
+	for my $trigger (@$triggers) {
+	    $trigger->($self, @_);
+	}
     }
     else {
-        # if validation is enabled we can only add valid trigger points
-        # so we only need to check in call_trigger() if there's no
-        # trigger with the requested name.
-        __validate_triggerpoint($self, $when);
-    }
-
-    return scalar @{$result_store->{'_class_trigger_results'}};
-}
-
-sub __fetch_all_triggers {
-    my ($obj, $when, $list, $order) = @_;
-    my $class = ref $obj || $obj;
-    my $return;
-    unless ($list) {
-        # Absence of the $list parameter conditions the creation of
-        # the unrolled list of triggers. These keep track of the unique
-        # set of triggers being collected for each class and the order
-        # in which to return them (based on hierarchy; base class
-        # triggers are returned ahead of descendant class triggers).
-        $list = {};
-        $order = [];
-        $return = 1;
-    }
-    no strict 'refs';
-    my @classes = @{$class . '::ISA'};
-    push @classes, $class;
-    foreach my $c (@classes) {
-        next if $list->{$c};
-        if (UNIVERSAL::can($c, 'call_trigger')) {
-            $list->{$c} = [];
-            __fetch_all_triggers($c, $when, $list, $order)
-                unless $c eq $class;
-            if (defined $when && $Triggers{$c}{$when}) {
-                push @$order, $c;
-                $list->{$c} = $Triggers{$c}{$when};
-            }
-        }
-    }
-    if ($return) {
-        my @triggers;
-        foreach my $class (@$order) {
-            push @triggers, @{ $list->{$class} };
-        }
-        if (ref $obj && defined $when) {
-            my $obj_triggers = $obj->{__triggers}{$when};
-            push @triggers, @$obj_triggers if $obj_triggers;
-        }
-        return @triggers;
+	# if validation is enabled we can only add valid trigger points
+	# so we only need to check in call_trigger() if there's no
+	# trigger with the requested name.
+	__validate_triggerpoint($self, $when);
     }
 }
 
 sub __validate_triggerpoint {
-    return unless my $points = $TriggerPoints{ref $_[0] || $_[0]};
+    return unless my $points = $_[0]->__triggerpoints;
     my ($self, $when) = @_;
     Carp::croak("$when is not valid triggerpoint for ".(ref($self) ? ref($self) : $self))
-        unless $points->{$when};
+	unless $points->{$when};
 }
 
 sub __fetch_triggers {
-    my ($obj, $proto) = @_;
+    my $proto = shift;
     # check object based triggers first
-    return ref $obj ? $obj->{__triggers} ||= {} : $Triggers{$obj} ||= {};
+    return (ref $proto and $proto->{__triggers}) || $proto->__triggers;
+}
+
+sub __update_triggers {
+    my($proto, $triggers) = @_;
+    if (ref $proto) {
+	# object attributes
+	$proto->{__triggers} = $triggers;
+    }
+    else {
+	# class data inheritable
+	$proto->__triggers($triggers);
+    }
+}
+
+sub __deep_dereference {
+    my $hashref = shift;
+    my %copy;
+    while (my($key, $arrayref) = each %$hashref) {
+	$copy{$key} = [ @$arrayref ];
+    }
+    return %copy;
 }
 
 1;
 __END__
 
-#line 349
+#line 276
 
